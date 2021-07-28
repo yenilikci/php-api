4c507fb831d61133c1e91aac71be3351cbaf82fb
yenilikci <m.melihcelik017@outlook.com>
2021-07-28 20:52:54 +0300

    Add token

--- api/index.php
+++ api/index.php
@@ -6,6 +6,9 @@ header("Access-Control-Allow-Origin: *");
 //set header
 header("Content-Type:application/json;charset=utf8");
 
+//secret key
+$token = "mySecretKey";
+
 require_once 'Configs/database.php';
 require_once 'Helpers/mainHelper.php';
 
@@ -16,16 +19,25 @@ $returnArray = [];
 //default status res
 $returnArray['status'] = false;
 
-$mode = $_GET["mode"];
-$process = $_GET["process"];
+//token check
+if (isset($_GET['token'])) {
+    if ($_GET['token'] == $token) {
+        $mode = $_GET["mode"];
+        $process = $_GET["process"];
 
-$path = 'Api/' . $mode . '/' . $process . '.php';
+        $path = 'Api/' . $mode . '/' . $process . '.php';
 
-//file control
-if (file_exists($path)) {
-    require_once 'Api/' . $mode . '/' . $process . '.php';
-    //convert json
-    echo json_encode($returnArray);
+        //file control
+        if (file_exists($path)) {
+            require_once 'Api/' . $mode . '/' . $process . '.php';
+            //convert json
+            echo json_encode($returnArray);
+        } else {
+            die("Page is not found!");
+        }
+    } else {
+        die("Token Hatalı!");
+    }
 } else {
-    die("Page is not found!");
+    die("Token Yok!");
 }
